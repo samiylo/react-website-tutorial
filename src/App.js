345bc5f70ba334cb1244f38cb13e919aa2b27244
samiylo <skryshu@gmail.com>
2024-03-05 13:00:04 -0600

    Rename Menu to Shop

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/menu" exact component={Menu} />
+          <Route path="/shop" exact component={Menu} />
           <Route path="/about" exact component={About} />
           <Route path="/contact" exact component={Contact} />
         </Switch>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,14 +16,14 @@ function Navbar() {
         <img src={Logo} />
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
-          <Link to="/menu"> Menu </Link>
+          <Link to="/shop"> Shop</Link>
           <Link to="/about"> About </Link>
           <Link to="/contact"> Contact </Link>
         </div>
       </div>
       <div className="rightSide">
         <Link to="/"> Home </Link>
-        <Link to="/menu"> Menu </Link>
+        <Link to="/shop"> Shop </Link>
         <Link to="/about"> About </Link>
         <Link to="/contact"> Contact </Link>
         <button onClick={toggleNavbar}>
